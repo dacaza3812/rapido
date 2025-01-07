23b33e361f5281c7faff7e8e04ada1efa75f113b
dacaza3812 <david383812@gmail.com>
2025-01-07 15:10:38 -0500

    creando la vista auth de customer

--- rapido-app/src/app/customer/auth.tsx
+++ rapido-app/src/app/customer/auth.tsx
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { authStyles } from '@/styles/authStyles'
+import { ScrollView } from 'react-native-gesture-handler'
+import { commonStyles } from '@/styles/commonStyles'
+import MaterialIcons from "@expo/vector-icons/MaterialIcons"
+import CustomText from '@/components/shared/CustomText'
+import PhoneInput from '@/components/shared/PhoneInput'
 
 const Auth = () => {
+    const [phone, setPhone] = useState("")
+
   return (
-    <View>
-      <Text>Customer Auth</Text>
-    </View>
+    <SafeAreaView style={authStyles.container}>
+        <ScrollView contentContainerStyle={authStyles.container}>
+
+            <View style={commonStyles.flexRowBetween}>
+                <Image source={require("@/assets/images/logo_t.png")} style={authStyles.logo}/>
+
+                <TouchableOpacity style={authStyles.flexRowGap}>
+                    <MaterialIcons name='help' size={18} color="grey"/>
+                    <CustomText fontFamily='Medium' variant='h7'>Ayuda</CustomText>
+                </TouchableOpacity>
+            </View>
+
+            <CustomText fontFamily='Medium' variant='h6'>
+                Cuál es tu número de teléfono?
+            </CustomText>
+
+            <CustomText variant='h7' fontFamily='Regular' style={commonStyles.lightText}>
+                Escriba su número de teléfono para proceder
+            </CustomText>
+
+            <PhoneInput
+            onChangeText={setPhone}
+            value={phone}
+            />
+
+            <View style={authStyles.footerContainer}>
+                <CustomText variant='h8' fontFamily='Regular' style={[commonStyles.lightText, {textAlign: "center", marginHorizontal: 20}]}>
+                    Al continuar, aceptas los términos y condiciones de Rapido
+                </CustomText>
+            </View>
+
+        </ScrollView>
+    </SafeAreaView>
   )
 }
 
