5fde870d7126f22bd1ff7192572499bf6fe4e26d
dacaza3812 <david383812@gmail.com>
2025-01-07 14:12:22 -0500

    creando la vista de role y layout

---
+++ rapido-app/src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler'
+
+const Layout = () => {
+  return (
+    <Stack screenOptions={{headerShown: false}}>
+        <Stack.Screen name='index' />
+        <Stack.Screen name='role' />
+    </Stack>
+  )
+}
+
+export default gestureHandlerRootHOC(Layout)
